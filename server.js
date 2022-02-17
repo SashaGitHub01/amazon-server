9814dce3eb78414f26a48dd63846c7b42897cc4f
SashaGitHub01 <sashab4b1n@yandex.ru>
2022-02-17 15:57:28 +0300

    fix3

--- server.js
+++ server.js
@@ -33,12 +33,12 @@ app.use(bodyParser.json({
 
 app.use('/api', router)
 
-if (process.env.NODE_ENV === 'production') {
-   app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-   app.get('*', function (req, res) {
-      res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
-   });
-}
+// if (process.env.NODE_ENV === 'production') {
+//    app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+//    app.get('*', function (req, res) {
+//       res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+//    });
+// }
 
 app.use(errorCatcher)
 
